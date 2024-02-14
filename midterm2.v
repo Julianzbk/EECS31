3b97ea329b28b57eaca528336a303c9ad80510dc
Julianzbk <62032232+Julianzbk@users.noreply.github.com>
2024-02-14 15:32:35 -0800

    Create midterm2.v

---
+++ midterm2.v
@@ -0,0 +1,33 @@
+`timescale 1ps/1ns
+module tb_something();
+    reg a, b;
+    wire s;
+    something instant(.a(a), .b(b), .s(s));
+    begin
+        
+    end
+endmodule
+
+always @(a)
+begin
+    case(a):
+        2'b01:
+        2'b10:
+        2'b11:
+        default:
+    endcase
+end
+
+        output reg [3:0] a;
+        output reg [3:0] b [0:2];
+parameter N = 2;
+time delay = 200;
+assign #delay a = b & c
+
+genvar i;
+generate
+    for (i = 0; i < N; i = i + 1)
+    begin
+        s[i] or whatever;
+    end
+endgenerate
